d286700eb4c8ecd7d41c4905492800659d9167c7
Óscar Lestón Casais <oscar.leston.casais@gmail.com>
2025-01-10 19:59:43 +0100

    modified:   core/config_file.php

--- core/config_file.php
+++ core/config_file.php
@@ -1,14 +1,14 @@
 <?php
 
-const SERVER_URL = "http://localhost:80/PsEduca-Backend";
+const SERVER_URL = "http://localhost:8080";
 const JWT_KEY = "jwt_key";
 
 // Database settings
-const DB_HOST = "localhost";//"192.168.1.254";
-const DB_PORT = "3306";
-const DB_NAME = "pseduca";
-const DB_USER = "pseduca";
-const DB_PASS = "pseduca";
+const DB_HOST = "localhost";
+const DB_PORT = 3306;
+const DB_NAME = "pseduca_DB";
+const DB_USER = "pseduca_user";
+const DB_PASS = "pseduca_pass";
 
 // Mail settings
 const MAIL_USER_NAME = 'moses.volkman@ethereal.email'; // name of user = Moses Volkman
@@ -20,12 +20,3 @@ const CONTACT_DESTINATION_DIR = 'correoDeSenhoran@uvigo.es';
 
 // File upload settings
 const UPLOAD_FOLDER = "uploads/";
-
-//const MAIL_USER_NAME = 'pseduca@outlook.es';
-//const MAIL_PASSWORD = 'EseiSoftwareSL2024';
-//const MAIL_HOST = 'smtp.office365.com';
-//const MAIL_PORT = 587;
-//const MAIL_OAUTH2_CLIENT_ID = '7a9c1bc6-5d61-4413-8ff5-aa6acac51440';
-//const MAIL_OAUTH2_TENANT_ID = '7dc1653e-e99a-479d-8e5d-f532eee7a373';
-//const MAIL_OAUTH2_CLIENT_SECRET = '9px8Q~Lt6OCidZAUZvVWtn~uUMPiZxkcwLmX-djU';
-//const MAIL_OAUTH2_REFRESH_TOKEN = 'M.C534_BAY.0.U.-CuSwfy52rWIpznl9HpoG5XU6qXv3rBMiDJPqBXbcS4FizmUJTgOqcKMys3dXjfcW1bdv9yHsqsTtoUhvZUoSAAWnAY61itjON4RP*dAypOMl4mHfs2pMd576qcZgQdn3oK6xR0ZGN4qKMTc5OOh*4bGWdK6c6l4YHmBlo*ef7enFjS9BUHBVC3!BsTjoHet7SMykp7qqMpz71czNoWCZfFGSRR6qAoYqjMX9ikGcLHBezXQwoH9ycr3iM8*LaS39dmsQQ9iSs*Ho07Xa*8CI57jMkNrSAgkHqog9IDCPnQ7Q5AbA2GnAodFBeOkla!5IeVkD4b3VOg*lpCrUTVrmIO0trQa0NhiRITqlLbnvRRuPW96Hz5Hu2LoeZOaBaoH2sm8wQjRT61eBfyq7!Ox19wA$';
